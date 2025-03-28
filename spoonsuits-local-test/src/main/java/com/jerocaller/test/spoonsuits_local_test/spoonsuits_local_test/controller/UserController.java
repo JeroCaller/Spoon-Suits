a67ba3fc12eb35c9561f1e949e81e55a95c95d01
JeroCaller <j69m17@gmail.com>
2025-03-28 20:33:07 +0900

    docs: 메인 패키지 내 특정 메서드들에 javadoc 작성

--- spoonsuits-local-test/src/main/java/com/jerocaller/test/spoonsuits_local_test/spoonsuits_local_test/controller/UserController.java
+++ spoonsuits-local-test/src/main/java/com/jerocaller/test/spoonsuits_local_test/spoonsuits_local_test/controller/UserController.java
@@ -5,6 +5,7 @@ import com.jerocaller.test.spoonsuits_local_test.spoonsuits_local_test.data.dto.
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test/users")
 public class UserController {
 
+    /**
+     * <p>
+     *     validationUtils#getValidationFailedMessage() 메서드 테스트를 위한 
+     *     컨트롤러 메서드. 
+     * </p>
+     * <p>
+     *     신규 유저 가입이란 상황을 가정하여 신규 유저 정보와 매핑될 
+     *     SiteUserRegisterDto 객체에 적용된 유효성 검사 실패 시 
+     *     실패한 필드명과 그 메시지를 생성하는지 보기 위함.
+     * </p>
+     * 
+     * 
+     * @see com.jerocaller.test.spoonsuits_local_test.spoonsuits_local_test.exception.GlobalExceptionHandler#handleValidationException(MethodArgumentNotValidException) 
+     * @param registerDto
+     * @return
+     */
     @PostMapping
     public ResponseEntity<RestResponse> register(
         @Valid @RequestBody SiteUserRegisterDto registerDto
--- spoonsuits-local-test/src/main/java/com/jerocaller/test/spoonsuits_local_test/spoonsuits_local_test/exception/GlobalExceptionHandler.java
+++ spoonsuits-local-test/src/main/java/com/jerocaller/test/spoonsuits_local_test/spoonsuits_local_test/exception/GlobalExceptionHandler.java
@@ -13,6 +13,19 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * <p>
+     *     ValidationUtils#getValidationFailedMessage() 메서드 테스트를 위한
+     *     예외 핸들러.
+     * </p>
+     * <p>
+     *     유효성 검사 실패 시 실패한 필드명과 그에 대한 메시지가 Map 형태로
+     *     반환되는지 확인하기 위한 용도.
+     * </p>
+     *
+     * @param e
+     * @return
+     */
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<RestResponse> handleValidationException(
         MethodArgumentNotValidException e
