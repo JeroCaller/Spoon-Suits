819a67cad7db5399bdf9e5cb33d65b972513c45f
JeroCaller <j69m17@gmail.com>
2025-03-28 16:08:58 +0900

    feat: 유효성 검사 실패 시 실패한 필드 정보를 반환하는 유틸리티 구현
    Resolves: #7

--- spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/ValidationUtils.java
+++ spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/ValidationUtils.java
@@ -1,4 +1,22 @@
 package com.jerocaller.libs.spoonsuits.web;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValidationUtils {
+
+    public static Map<String, String> getValidationFailedMessage(
+        MethodArgumentNotValidException error
+    ) {
+
+        Map<String, String> validationFailedMap = new HashMap<>();
+        error.getBindingResult().getFieldErrors().forEach(e -> {
+            validationFailedMap.put(e.getField(), e.getDefaultMessage());
+        });
+
+        return validationFailedMap;
+    }
+
 }
