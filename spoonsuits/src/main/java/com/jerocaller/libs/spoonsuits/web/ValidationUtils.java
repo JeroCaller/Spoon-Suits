dbbe5d1fd95ba41b2ce658fb078e255191039c11
JeroCaller <j69m17@gmail.com>
2025-03-28 16:55:54 +0900

    docs: 유효성 검사 유틸리티 클래스에 javadoc 추가
    메서드 파라미터명 변경. 메서드 파라미터명 변경과 그로 인한 javadocs의 param 문서 내용 변경이
    연결되어 있어 함께 진행함.
    
    Resolves: #23

--- spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/ValidationUtils.java
+++ spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/ValidationUtils.java
@@ -5,14 +5,35 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * <p>유효성 검사 유틸리티 클래스</p>
+ *
+ * <p>
+ *     이 클래스 내 모든 메서드들은 static으로 선언되어 있기에
+ *     별도로 Spring Bean으로 등록하는 과정은 불필요하다.
+ * </p>
+ */
 public class ValidationUtils {
 
+    /**
+     * <p>
+     *     유효성 검사에 실패한 필드들의 정보만 추출하여
+     *     <code>필드명: 유효성 검사 실패 메시지</code>의 Map 타입으로 반환하는 메서드.
+     * </p>
+     * <p>
+     *     REST API에서 특정 필드들의 유효성 검사 실패 시 응답 JSON에 포함시킬 목적으로
+     *     만든 메서드.
+     * </p>
+     *
+     * @param exception 유효성 검사 실패로 발생한 MethodArgumentNotValidException 예외 객체
+     * @return <code>필드명: 유효성 검사 실패 메시지</code> 형태의 Map 타입
+     */
     public static Map<String, String> getValidationFailedMessage(
-        MethodArgumentNotValidException error
+        MethodArgumentNotValidException exception
     ) {
 
         Map<String, String> validationFailedMap = new HashMap<>();
-        error.getBindingResult().getFieldErrors().forEach(e -> {
+        exception.getBindingResult().getFieldErrors().forEach(e -> {
             validationFailedMap.put(e.getField(), e.getDefaultMessage());
         });
 
