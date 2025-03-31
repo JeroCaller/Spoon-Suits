be4704fe4205ed3a0a811e0d057859d96c2b36ee
JeroCaller <j69m17@gmail.com>
2025-03-31 11:30:26 +0900

    feat: 기본 JWT 인증 필터 구현
    Resolves: #8

--- spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/jwt/DefaultJwtAuthenticationFilter.java
+++ spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/jwt/DefaultJwtAuthenticationFilter.java
@@ -1,5 +1,41 @@
 package com.jerocaller.libs.spoonsuits.web.jwt;
 
-// TODO : 구현
-public class DefaultJwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class DefaultJwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String accessToken = jwtAuthenticationProvider.resolveToken(request);
+
+        if (accessToken != null &&
+            jwtAuthenticationProvider.validateToken(accessToken)) {
+            Authentication auth = jwtAuthenticationProvider
+                .getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
 }
