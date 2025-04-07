c77c958e98f2f1a8839e701e8a9fd441d2e94d94
JeroCaller <j69m17@gmail.com>
2025-04-07 15:29:57 +0900

    feat: Page 유틸리티 구현.
    Javadoc도 함께 작성.
    Page 객체 내 요소들의 존재 여부 확인 기능 추가.
    
    Resolves: #34

---
+++ spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/jpa/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>Spring Data JPA 관련 도구 모음</p>
+ *
+ * <ul>
+ *     <li><b>Required Dependencies in Gradle(Groovy)</b></li>
+ * </ul>
+ * <pre><code>
+ *     implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
+ * </code></pre>
+ */
+package com.jerocaller.libs.spoonsuits.web.jpa;
