11fb9b373964d8ca0dcb9d73c2b17623e0c38a6e
JeroCaller <j69m17@gmail.com>
2025-04-07 22:30:39 +0900

    test: Page 유틸리티에 대한 테스트 코드 작성 및 테스트 성공
    Resolves: #35

---
+++ spoonsuits-local-test/src/main/java/com/jerocaller/test/spoonsuits_local_test/spoonsuits_local_test/data/repository/FestivalRepository.java
@@ -0,0 +1,7 @@
+package com.jerocaller.test.spoonsuits_local_test.spoonsuits_local_test.data.repository;
+
+import com.jerocaller.test.spoonsuits_local_test.spoonsuits_local_test.data.entity.Festival;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FestivalRepository extends JpaRepository<Festival, Integer> {
+}
