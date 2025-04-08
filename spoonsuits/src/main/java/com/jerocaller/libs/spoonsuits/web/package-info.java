878b78278070e980cc26e6f5772d017b39e6f12d
JeroCaller <j69m17@gmail.com>
2025-04-08 15:27:29 +0900

    docs: web 패키지의 설명을 담은 package-info 추가

---
+++ spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/web/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * <p>
+ *     Spring Boot를 이용하여 웹 애플리케이션 구현 시
+ *     필요한 기능들을 모은 패키지.
+ * </p>
+ * <ul>
+ *     <li><b>Commonly required dependencies in Gradle(Groovy)</b></li>
+ * </ul>
+ * <pre><code>
+ *   // lombok
+ *   compileOnly 'org.projectlombok:lombok'
+ *   annotationProcessor 'org.projectlombok:lombok'
+ *
+ *   // web
+ *   implementation 'org.springframework.boot:spring-boot-starter-web'
+ * </code></pre>
+ */
+package com.jerocaller.libs.spoonsuits.web;
