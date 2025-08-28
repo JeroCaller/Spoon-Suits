8e42e5222336f8fbe7f07476c254189622cf622a
JeroCaller <j69m17@gmail.com>
2025-08-28 23:31:39 +0900

    refactor: 환경설정값 및 자바 클래스간 바인딩 방식을 setter에서 생성자 바인딩 방식으로 리팩토링.
    관련 로컬 테스트 완료 및 모두 성공.

--- spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/config/AutoConfig.java
+++ spoonsuits/src/main/java/com/jerocaller/libs/spoonsuits/config/AutoConfig.java
@@ -1,10 +1,13 @@
 package com.jerocaller.libs.spoonsuits.config;
 
+import com.jerocaller.libs.spoonsuits.web.jwt.JwtProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
 @ComponentScan("com.jerocaller.libs.spoonsuits")
+@EnableConfigurationProperties({JwtProperties.class})
 public class AutoConfig {
 
 }
